cb8d387ea881d82adeeccb6ab910a74f1e987f7d
richard-shepherd <richard.s.shepherd@gmail.com>
2017-01-26 22:04:57 +0000

    hf

---
+++ www/js/TextureManager.js
@@ -0,0 +1,89 @@
+/**
+ * TextureManager
+ * --------------
+ * Loads textures which can be applied to 3D objects.
+ *
+ * This class is a singleton.
+ */
+function TextureManager() {
+    // The number of textures we manage...
+    this._numTextures = 0;
+    this._texturesLoaded = 0;
+
+    // The three.js texture-loader...
+    this._textureLoader = new THREE.TextureLoader();
+
+    // Called back as textures are loaded...
+    this._progressCallback = null;
+
+    // The collection of textures...
+    this._textures = {};
+}
+
+/**
+ * An enum for the textures we manage.
+ */
+TextureManager.TextureType = {
+    SHOTGUN: 1,
+    IMP : 2
+};
+
+/**
+ * The singleton instance.
+ */
+TextureManager._instance = null;
+
+/**
+ * getInstance
+ * -----------
+ * Gets the singleton instance.
+ */
+TextureManager.getInstance = function() {
+    if(TextureManager._instance === null) {
+        TextureManager._instance = new TextureManager();
+    }
+    return TextureManager._instance;
+};
+
+/**
+ * initialize
+ * ----------
+ */
+TextureManager.prototype.initialize = function(progressCallback) {
+    this._progressCallback = progressCallback;
+    this._loadTexture(TextureManager.TextureType.IMP, "imp.png")
+    this._loadTexture(TextureManager.TextureType.SHOTGUN, "shotgun.png")
+};
+
+/**
+ * getTexture
+ * ----------
+ * Returns the texture requested.
+ */
+TextureManager.prototype.getTexture = function(textureType) {
+    return this._textures[textureType];
+};
+
+/**
+ * _loadTexture
+ * ------------
+ * Loads the texture requested from the textures folder and calls
+ * back when it is loaded.
+ */
+TextureManager.prototype._loadTexture = function(textureType, filename) {
+    var that = this;
+    this._numTextures++;
+    var path = "textures/" + filename;
+    this._textureLoader.load(path, function(texture) {
+        that._textures[textureType] = texture;
+        that._texturesLoaded++;
+        if(that._progressCallback !== null) {
+            var info = {
+                text: "Loading textures",
+                total: that._numTextures,
+                loaded: that._texturesLoaded
+            };
+            that._progressCallback(info);
+        }
+    });
+};
